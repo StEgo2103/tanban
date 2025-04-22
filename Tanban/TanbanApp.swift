09ecc5fd2f1219900bffdcad71c84b5a46eea321
StEgo2103 <luca@deltort.net>
2025-04-21 17:36:09 -0700

    Initial Commit

---
+++ Tanban/TanbanApp.swift
@@ -0,0 +1,32 @@
+//
+//  TanbanApp.swift
+//  Tanban
+//
+//  Created by Luca on 21/04/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct TanbanApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ TanbanTests/TanbanTests.swift
@@ -0,0 +1,17 @@
+//
+//  TanbanTests.swift
+//  TanbanTests
+//
+//  Created by Luca on 21/04/2025.
+//
+
+import Testing
+@testable import Tanban
+
+struct TanbanTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
